b45d894b96a17e3320675609660a4696e0af5563
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2024-11-21 17:35:03 +0100

    path correction

--- php/programme/prog_action.php
+++ php/programme/prog_action.php
@@ -40,12 +40,12 @@ function supprimer() {
     $params = getParams(["lien"]);
     if($params == null)return;
 
-    $dir = (String) "../pdf/".$params['lien'];
+    $dir = (String) "../../pdf/".$params['lien'];
 
     // si c'est un fichier :
     if(!file_exists($dir))return set_error("Le fichier n'existe pas");
 
-    if(!supr($dir, "../delete/".$params['lien'], true))set_error("Erreur lors de la suppression");
+    if(!supr($dir, "../../delete/".$params['lien'], true))set_error("Erreur lors de la suppression");
     
     echo "success";
 }
