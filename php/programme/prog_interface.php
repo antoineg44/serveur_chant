c20cf6126081de471953b1bb7dd93b9ba43dda12
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2024-11-21 16:14:26 +0100

    test

--- php/programme/prog_interface.php
+++ php/programme/prog_interface.php
@@ -4,11 +4,16 @@ include("../res/generic.php");
 include("../res/file_lib.php");
 include("prog_action.php");
 
+echo "start";
+
 $params = getParams(["action"]);
 if($params == null)return;
 
+echo "suite";
+
 switch ($params['action']) {
     case 'nouveau':
+        echo 'nouveau';
         nouveau();
         break;
     
@@ -16,5 +21,6 @@ switch ($params['action']) {
         # code...
         break;
 }
+echo "fin";
 
 ?>
