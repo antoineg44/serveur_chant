9a08887edf1cedb184427c142cff47a9959c4c6f
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2024-11-21 15:56:29 +0100

    test

--- php/programme/nouveau_prog.php
+++ php/programme/nouveau_prog.php
@@ -9,7 +9,6 @@ if($params == null)return;
 
 $auteur = $params["auteur"];    // useless at the moment
 
-
 // Get original file
 $old_link = (String) "../../pdf/".params['old_link'];
 $decoded = json_decode(file_get_contents($old_link),true);     // Get json informations
@@ -22,25 +21,19 @@ $nom = $params['nom'];
 $decoded["path_file"] = "https://".$_SERVER['HTTP_HOST']."/pdf/".params['old_link'];
 
 $nom_sans_ext = explode(".", $nom);
-if($nom_sans_ext[1] != "json"){
-    echo "Problème sur le nom du fichier";
-    return;
-}
+if($nom_sans_ext[1] != "json")return set_error("Problème sur le nom du fichier");
+
 $param = explode("_", $nom_sans_ext[0]);
-if(sizeof($param) != 3) {
-    echo "Nom incorrect";
-    return;
-}
+if(sizeof($param) != 3)return set_error("Nom incorrect");
 $decoded["date"] = $param[0];
 $decoded["lieu"] = $param[1];
 $decoded["occasion"] = $param[2];
 $decoded["dateLastModif"] = date("YmdHis");     // Date de la dernière modification
 
-if(file_exists('../../pdf/programmes/'.$decoded["paroisse"]."/".$nom)) {
-    echo "file already exist";
-    return;
-}
+// vérification de l'existance du programme
+if(file_exists('../../pdf/programmes/'.$decoded["paroisse"]."/".$nom))return set_error("file already exist");
 
+// enregistrement du nouveau programme
 write_program_json($decoded["paroisse"]."/".$nom, $decoded);
 
 echo "success";
--- php/res/generic.php
+++ php/res/generic.php
@@ -1,19 +1,25 @@
 <?php
 
+function set_error($message)
+{
+    echo $message;
+    return null;
+}
+
 function getParams($input)
 {
     $success = true;
 
     for($i=0; $success && $i<sizeof($input); $i++) {
-        if(isset($_GET[$input[i]]) && !empty($_GET[$input[i]])) {
+        if(isset($_GET[$input[$i]]) && !empty($_GET[$input[$i]])) {
             $success = false;
         } else {
-            $params[$input[i]] = (String)trim($_GET[$input[i]]);
+            $params[$input[$i]] = (String)trim($_GET[$input[$i]]);
         }
     }
 
     if($success == false) {
-        echo "params failure";
+        set_error("params failure");
         return null;
     }
 
