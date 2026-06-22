7c79dda0fc033faf726460a0aeb6d8abfbaec816
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2026-06-22 14:02:04 +0200

    Resolve defect path

--- components/autocomplete/autocomplete.js
+++ components/autocomplete/autocomplete.js
@@ -56,7 +56,8 @@ var ac = {
           inst.hList.innerHTML = "";
           inst.hList.style.display = "none";
           const parsed = new URL(pastedText);
-          var new_path = decodeURI(parsed.pathname.slice(5) + parsed.search + parsed.hash);
+          // resolve defect virgule when copy and paste url: .replace("%2C", ",")
+          var new_path = (decodeURI(parsed.pathname.slice(5) + parsed.search + parsed.hash)).replace("%2C", ",");
           console.log("parsed path: " + new_path);
           inst.timer = setTimeout(() => {
             //inst.target.value =new_path;
